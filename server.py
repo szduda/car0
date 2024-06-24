127f1a9db443b15d5b605a855eb5f673a0a5edcb
szduda <szymon.piotr.duda@gmail.com>
2024-06-25 01:33:22 +0200

    setup microdot server with ws an sse communication

---
+++ server.py
@@ -0,0 +1,82 @@
+from microdot.wsgi import Microdot, send_file, with_websocket
+from microdot.sse import with_sse
+from asyncio import sleep
+
+from drive import Drive
+from vehicleMonitor import BatteryMonitor
+
+speed = 100
+drive = Drive(12, 18, 13, 19)
+
+app = Microdot()
+battery_monitor = BatteryMonitor(i2c_bus=1)
+
+
+@app.route('/')
+async def index(request):
+  return send_file('static/index.html')
+
+
+@app.route('/monitor')
+@with_sse
+async def monitor(request, sse):
+  while True:
+    voltage, voltage_percent = battery_monitor.get_voltage()
+    current, current_percent = battery_monitor.get_current()
+    h, m = battery_monitor.get_time_until_discharge(voltage_percent, current)
+    await sleep(1)
+    await sse.send({voltage, voltage_percent, current, current_percent, h, m})
+
+
+@app.route('/steer')
+@with_websocket
+async def steer(request, ws):
+  while True:
+    cmd = await ws.receive()
+    print(f'[{cmd}] command received')
+
+    async def ok(): await ws.send('ok')
+
+    global speed
+    if cmd in map(str, range(3, 10)):
+      speed = (int(cmd) + 1) * 10
+      await ok()
+      continue
+
+    match cmd:
+      case 'fwd':
+        drive.fwd(speed)
+        await ok()
+      case 'rev':
+        drive.rev(speed)
+        await ok()
+      case 'rtl':
+        drive.turn('l', speed)
+        await ok()
+      case 'rtr':
+        drive.turn('r', speed)
+        await ok()
+      case 'stp':
+        drive.stop()
+        await ok()
+      case 'bye':
+        drive.deinit()
+        await ws.send('farewell')
+      case _:
+        await ws.send('unknown command')
+
+
+@app.route("/static/<path:path>")
+def static(request, path):
+    if ".." in path:
+        # directory traversal is not allowed
+        return "Not found", 404
+    return send_file("static/" + path)
+
+
+if __name__ == '__main__':
+  try:
+    print("Starting the server...")
+    app.run()
+  except KeyboardInterrupt:
+    drive.deinit()
---
+++ static/index.js
@@ -0,0 +1,24 @@
+const log = (text, color) => {
+  document.getElementById('debugOut').innerHTML += `<span style="color: ${color}">${text}</span><br/>`;
+};
+
+const socket = new WebSocket('ws://' + location.host + '/steer');
+socket.addEventListener('message', ev => {
+  log('<<< ' + ev.data, 'blue');
+});
+socket.addEventListener('close', ev => {
+  log('<<< WS closed');
+});
+
+const sse = new EventSource(`//${location.host}/monitor`)
+sse.onopen(() => log('>>> SSE opened'))
+sse.onmessage((e) => log(`>>> SSE message: ${e.data}`))
+sse.onerror(() => log('>>> SSE error; reconnecting'))
+
+document.getElementById('form').onsubmit = ev => {
+  ev.preventDefault();
+  const input = document.getElementById('cmdInput');
+  log('>>> ' + input.value, 'red');
+  socket.send(input.value);
+  input.value = '';
+};
