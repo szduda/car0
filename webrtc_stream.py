a6521481e7ab55a397d741bedfe2d05e910e4200
Szymon Duda <szymon.duda@sofomo.com>
2025-11-18 11:48:40 +0100

    webrtc camera stream

--- webrtc_stream.py
+++ webrtc_stream.py
@@ -1,92 +1,108 @@
-import json
-from aiohttp import web
-from aiortc import RTCPeerConnection, VideoStreamTrack
-from aiortc.contrib.media import MediaBlackhole, MediaRelay
-from picamera2 import Picamera2
-import av
 import threading
 import time
 
-# Start camera
+from aiohttp import web
+from aiortc import RTCPeerConnection, VideoStreamTrack, RTCSessionDescription
+from aiortc.codecs.h264 import H264Frame
+
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import Output
+
+# Custom output class for H264Encoder
+class InMemoryOutput(Output):
+    def __init__(self):
+        super().__init__()
+        self.buffer = b""
+        self.lock = threading.Lock()
+
+    def write(self, buf):
+        # Picamera2 will call this with encoded H264 data (NAL units)
+        with self.lock:
+            self.buffer += buf
+
+    def read(self):
+        # Return and clear the buffer
+        with self.lock:
+            data = self.buffer
+            self.buffer = b""
+        return data
+
+# Setup camera
 picam2 = Picamera2()
-config = picam2.create_video_configuration(
-    main={"size": (1280, 720)},
-    encode="h264"
-)
-picam2.configure(config)
+video_config = picam2.create_video_configuration(main={"size": (1280, 720)}, encode="h264")
+picam2.configure(video_config)
+
+output = InMemoryOutput()
+encoder = H264Encoder()
+
+picam2.start_recording(encoder, output)
 picam2.start()
 
 # Shared frame buffer
-current_frame = None
-frame_lock = threading.Lock()
+current_nal = b""
+nal_lock = threading.Lock()
 
-# Background camera capture to memory
-def capture_frames():
-    global current_frame
+# Thread: constantly read from encoder output
+def reader_thread():
+    global current_nal
     while True:
-        with frame_lock:
-            current_frame = picam2.capture_buffer("main")  # returns H.264 encoded buffer
+        data = output.read()
+        if data:
+            with nal_lock:
+                current_nal = data
         time.sleep(0.01)
 
-threading.Thread(target=capture_frames, daemon=True).start()
+threading.Thread(target=reader_thread, daemon=True).start()
 
-
-# Track that feeds frames into WebRTC
+# WebRTC VideoTrack
 class CameraTrack(VideoStreamTrack):
     kind = "video"
 
-    def __init__(self):
-        super().__init__()
-
     async def recv(self):
         pts, time_base = await self.next_timestamp()
-
-        with frame_lock:
-            data = current_frame
-
-        # Decode H264 into a frame for WebRTC
-        packet = av.Packet(data)
-        frame = None
-        for f in av.codec.CodecContext.create("h264", "r").decode(packet):
-            frame = f
-            break
-
+        with nal_lock:
+            nal = current_nal
+        # Wrap into H264Frame for aiortc
+        frame = H264Frame(nal)
         frame.pts = pts
         frame.time_base = time_base
         return frame
 
-
 pcs = set()
 routes = web.RouteTableDef()
-
 
 @routes.post("/offer")
 async def offer(request):
     params = await request.json()
+    desc = params["offer"]
+    _offer = RTCSessionDescription(sdp=desc["sdp"], type=desc["type"])
 
     pc = RTCPeerConnection()
     pcs.add(pc)
 
     @pc.on("iceconnectionstatechange")
-    def on_ice_change():
+    def on_ice():
+        print("ICE state:", pc.iceConnectionState)
         if pc.iceConnectionState == "failed":
-            pc.close()
             pcs.discard(pc)
+            pc.close()
 
-    # Add video track
     pc.addTrack(CameraTrack())
 
-    # Handle offer
-    await pc.setRemoteDescription(params['offer'])
+    await pc.setRemoteDescription(_offer)
     answer = await pc.createAnswer()
     await pc.setLocalDescription(answer)
 
-    return web.Response(
-        content_type="application/json",
-        text=json.dumps({"answer": pc.localDescription})
-    )
-
+    return web.json_response({
+        "answer": {
+            "sdp": pc.localDescription.sdp,
+            "type": pc.localDescription.type
+        }
+    })
 
 app = web.Application()
 app.add_routes(routes)
-web.run_app(app, port=5001)
+
+if __name__ == "__main__":
+    web.run_app(app, port=5001)
